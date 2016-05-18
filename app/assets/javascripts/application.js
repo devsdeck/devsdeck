718b5a2c6a6da8f91bfb1cc7584caeae1c127291
Aaron Myatt <aaronmyatt@gmail.com>
2016-05-18 10:56:26 +0800

    add sementic ui js dependency

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require jquery.infinite-pages
+//= require semantic-ui
 //= require_tree .
-
