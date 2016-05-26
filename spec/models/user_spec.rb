83752ce0d1d8a2f32feaec26c09810c8926d0d74
hosseintoussi <hossein@mindvalley.com>
2016-05-26 11:01:36 +0800

    removed trailing white spaces

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,17 +7,17 @@ RSpec.describe User, type: :model do
 
   describe '#latest_cards' do
     it 'should display the lastet cards in a DESC order' do
-      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)      
-      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)      
-      
+      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)
+      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)
+
       expect(user.latest_cards.first.name).to eq('card_two')
     end
   end
 
   describe '#latest_likes' do
     it 'should display the lastet cards user liked in a DESC order' do
-      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)      
-      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)      
+      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)
+      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)
       card1.liked_by(user)
       card2.liked_by(user)
 
@@ -26,22 +26,22 @@ RSpec.describe User, type: :model do
   end
 
   describe '#total_cards' do
-    it 'should return the total number of cards by the user' do 
-      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)      
-      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)      
-      
+    it 'should return the total number of cards by the user' do
+      FactoryGirl.create(:card, name: 'card_one', user: user)
+      FactoryGirl.create(:card, name: 'card_two', user: user)
+
       expect(user.total_cards).to eq(2)
-    end 
+    end
   end
 
   describe '#total_likes' do
-    it 'should return the total number of cards liked by the user' do 
-      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)      
-      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)      
+    it 'should return the total number of cards liked by the user' do
+      card1 = FactoryGirl.create(:card, name: 'card_one', user: user)
+      card2 = FactoryGirl.create(:card, name: 'card_two', user: user)
       card1.liked_by(user)
       card2.liked_by(user)
 
       expect(user.total_likes).to eq(2)
-    end 
+    end
   end
 end
