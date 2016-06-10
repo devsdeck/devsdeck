8f90015e5a8265b9e48492935251a175022ec9e7
hosseintoussi <hossein@mindvalley.com>
2016-06-10 15:51:04 +0800

    clean ups and small refactorings

--- app/workers/postman_worker.rb
+++ app/workers/postman_worker.rb
@@ -1,12 +1,17 @@
 class PostmanWorker
   include Sidekiq::Worker
+  sidekiq_options retry: false
 
   def perform
-    cards = Card.where("created_at >= ?", Time.zone.now.beginning_of_day)
+    cards = Card.where("created_at >= ?", 24.hours.ago)
     return if cards.blank?
 
     User.all.each do |user|
-      CardsMailer.daily_mail(user, cards).deliver_now
+      begin
+        CardsMailer.daily_mail(user, cards).deliver_now
+      rescue StandardError => error
+        logger.error("failed to send email for #{user.email}")
+      end
     end
   end
 end
