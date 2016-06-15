250e8b76ebd1ccb62b989c6153a4a360f9e6df9f
Aaron Myatt <aaronmyatt@gmail.com>
2016-06-15 22:04:47 +0800

    tag generator stuff

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -1,25 +1,25 @@
 (function(){
     console.log('cards.js');
 
-    $(document).ready(function(){
-        console.log('Set click handlers');
-
-        $('a.ui.tag.label').on('click', search_by_tag);
-    });
-
-    function search_by_tag(data){
-        console.log('Tag clicked: ', data.target.innerText);
-        pass_text_to_search_field(data.target.innerText);
-        submit_form('card_search');
-    }
-
-    function pass_text_to_search_field(text){
-        $('#q_name_cont')[0].value = text;
-    }
-
-    function submit_form(id){
-        $('#'+id)[0].submit();
-    }
+    // $(document).on("page:change", function(){
+    //     console.log('Set click handlers');
+    //
+    //     $('a.ui.tag.label').on('click', search_by_tag);
+    // });
+    //
+    // function search_by_tag(data){
+    //     console.log('Tag clicked: ', data.target.innerText);
+    //     pass_text_to_search_field(data.target.innerText);
+    //     submit_form('card_search');
+    // }
+    //
+    // function pass_text_to_search_field(text){
+    //     $('#q_name_cont')[0].value = text;
+    // }
+    //
+    // function submit_form(id){
+    //     $('#'+id)[0].submit();
+    // }
 
     $( document ).on("page:change", function () {
         if ($(".pagination").length) {
