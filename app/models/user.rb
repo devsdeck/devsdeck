7ef2377444d982ef27f66eb456b360de4650e382
Jimmy Ngu <jimmynguyc@gmail.com>
2016-06-24 15:07:52 +0800

    Fix indentation

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,8 +26,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, 
-		 :validatable, :confirmable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :confirmable
   devise :omniauthable, :omniauth_providers => [:github]
 
   has_many :cards
