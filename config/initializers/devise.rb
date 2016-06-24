c73ac7251bcc6673b00497c298c913c654a41543
Jimmy Ngu <jimmynguyc@gmail.com>
2016-06-24 15:15:39 +0800

    Align the parameters of a method call if they span more than one line.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -239,7 +239,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   config.omniauth :github,
-    ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user,public_repo"
+    ENV["GITHUB_KEY"],
+    ENV["GITHUB_SECRET"],
+    scope: "user,public_repo"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
