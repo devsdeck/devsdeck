ef6a54cbc275076e35b4e983953f409b20dfa757
Jimmy Ngu <jimmynguyc@gmail.com>
2016-06-24 15:14:53 +0800

    Line length fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'tags/:id/cards' => 'tags#cards', as: 'tag_cards'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   resources :users
 
   resources :cards do
