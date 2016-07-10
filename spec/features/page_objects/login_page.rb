7393a62fd9d4d042e4f9dbd6426efd0fb40aa36f
hosseintoussi <hossein@mindvalley.com>
2016-07-10 13:59:28 +0800

    minor changes to make syntaxes look the same

--- spec/features/page_objects/login_page.rb
+++ spec/features/page_objects/login_page.rb
@@ -4,10 +4,10 @@ class LoginPage
   include Capybara::DSL
 
   def login(email:, password:)
-    visit('/')
+    visit("/")
     click_link "Login"
     fill_in "user_email", with: email
     fill_in "user_password", with: password
-    click_button "Log in"
+    click_button("Log in")
   end
 end
