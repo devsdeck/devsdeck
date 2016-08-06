a392cf02bfb5b053d8c46f446fd78641e0286ad1
hosseintoussi <hossein@mindvalley.com>
2016-08-06 23:00:00 +0800

    updated permissions so everyone can see user cards and likes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,7 +10,7 @@ class Ability
       can [:update], Card do |card|
         card.owned_by?(user)
       end
-      can [:read], User
+      can [:read, :cards, :likes], User
       can [:update], User do |userr|
         userr == user
       end
