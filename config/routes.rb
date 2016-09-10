603e61df273529461522bc8ca91b0ecdaf7ea5fb
Jimmy Ngu <jimmynguyc@gmail.com>
2016-09-10 13:15:54 +0800

    hound-bot comments

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   end
 
   resources :cards do
-    patch 'like_or_unlike'
+    patch "like_or_unlike"
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
