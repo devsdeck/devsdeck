0e9730fe42015e774b94a5df55475e5c50974d34
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-04-11 21:06:35 +0000

    Rails 5 "controllers specs": new request syntax.

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe CardsController, type: :controller do
   describe 'GET #show' do
     it 'assigns the requested card to @card' do
       card = Card.create(valid_attributes)
-      get :show, id: card.to_param
+      get :show, params: { id: card.to_param }
 
       expect(assigns(:card)).to eq(card)
     end
@@ -34,7 +34,7 @@ RSpec.describe CardsController, type: :controller do
 
   describe 'GET #new' do
     it 'assigns a new card as @card' do
-      get :new, {}
+      get :new
       expect(assigns(:card)).to be_a_new(Card)
     end
   end
@@ -42,7 +42,7 @@ RSpec.describe CardsController, type: :controller do
   describe 'GET #edit' do
     it 'assigns the requested card as @card' do
       card = Card.create! valid_attributes
-      get :edit, id: card.to_param
+      get :edit, params: { id: card.to_param }
       expect(assigns(:card)).to eq(card)
     end
   end
@@ -50,30 +50,31 @@ RSpec.describe CardsController, type: :controller do
   describe 'POST #create' do
     context 'with valid params' do
       it 'creates a new Card' do
-        expect { post :create, card: valid_attributes }
-            .to change(Card, :count).by(1)
+        expect {
+          post :create, params: { card: valid_attributes }
+        }.to change(Card, :count).by(1)
       end
 
       it 'assigns a newly created card as @card' do
-        post :create, card: valid_attributes
+        post :create, params: { card: valid_attributes }
         expect(assigns(:card)).to be_a(Card)
         expect(assigns(:card)).to be_persisted
       end
 
       it 'redirects to the created card' do
-        post :create, card: valid_attributes
+        post :create, params: { card: valid_attributes }
         expect(response).to redirect_to(Card.last)
       end
     end
 
     context 'with invalid params' do
       it 'assigns a newly created but unsaved Card as @card' do
-        post :create, card: invalid_attributes
+        post :create, params: { card: invalid_attributes }
         expect(assigns(:card)).to be_a_new(Card)
       end
 
       it 're-renders the new template' do
-        post :create, card: invalid_attributes
+        post :create, params: { card: invalid_attributes }
         expect(response).to render_template('new')
       end
     end
@@ -87,25 +88,25 @@ RSpec.describe CardsController, type: :controller do
 
       it 'updates the requested card' do
         card = Card.create! valid_attributes
-        put :update, id: card.to_param, card: new_attributes
+        put :update, params: { id: card.to_param, card: new_attributes }
         card.reload
       end
 
       it 'assigns the requested card as @card' do
         card = Card.create! valid_attributes
-        put :update, id: card.to_param, card: valid_attributes
+        put :update, params: { id: card.to_param, card: valid_attributes }
         expect(assigns(:card)).to eq(card)
       end
 
       it 'redirects to root because user does not own the card' do
         card = Card.create! valid_attributes
-        put :update, id: card.to_param, card: valid_attributes
+        put :update, params: { id: card.to_param, card: valid_attributes }
         expect(response).to redirect_to(root_path)
       end
 
       it 'redirects to card' do
         card = user.cards.create! valid_attributes
-        put :update, id: card.to_param, card: valid_attributes
+        put :update, params: { id: card.to_param, card: valid_attributes }
         expect(response).to redirect_to(card)
       end
     end
@@ -113,13 +114,13 @@ RSpec.describe CardsController, type: :controller do
     context 'with invalid params' do
       it 'assigns the card as @card' do
         card = Card.create! valid_attributes
-        put :update, id: card.to_param, card: invalid_attributes
+        put :update, params: { id: card.to_param, card: invalid_attributes }
         expect(assigns(:card)).to eq(card)
       end
 
       it 're-renders the edit template' do
         card = user.cards.create! valid_attributes
-        put :update, id: card.to_param, card: invalid_attributes
+        put :update, params: { id: card.to_param, card: invalid_attributes }
         expect(response).to render_template('edit')
       end
     end
@@ -128,13 +129,14 @@ RSpec.describe CardsController, type: :controller do
   describe 'DELETE #destroy' do
     it 'does not the requested card' do
       card = Card.create! valid_attributes
-      expect { delete :destroy, id: card.to_param }
-          .to change(Card, :count).by(0)
+      expect {
+        delete :destroy, params: { id: card.to_param }
+      }.to change(Card, :count).by(0)
     end
 
     it 'redirects to the card list' do
       card = Card.create! valid_attributes
-      delete :destroy, id: card.to_param
+      delete :destroy, params: { id: card.to_param }
       expect(response).to redirect_to(root_path)
     end
   end
