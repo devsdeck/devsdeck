84c03be3b474f79fd88a89ff5d423a5499ce21d0
hosseintoussi <sltoussi@gmail.com>
2017-05-02 14:49:56 +0200

    refactored clipboard js file for conventions

--- app/assets/javascripts/clipboard.js
+++ app/assets/javascripts/clipboard.js
@@ -7,31 +7,32 @@ function addCopyButton() {
 
   $("pre").each(function(index) {
     var id = "id_" + index;
-    $(this).children("code").attr("id", id);
-    prependButtonAndHide($(this), id);
-    toggleCopyButton($(this));
+    var $this = $(this)
+    $this.children("code").attr("id", id);
+    prependButtonAndHide($this, id);
+    toggleCopyButton($this);
   });
 }
 
-function prependButtonAndHide(pre, id) {
-  if (pre.find(".clipboard-copy").length == 0) {
+function prependButtonAndHide($pre, id) {
+  if ($pre.find(".clipboard-copy").length === 0) {
     var button = $("<div class='ui small basic icon buttons right floated'>" +
       "<button class='mini ui button clipboard-copy' data-clipboard-target=#" + id + ">" +
       "<i class='clipboard icon'></i>" +
       "</button>" +
       "</div>");
-    pre.children("code").prepend(button);
-    pre.find(".clipboard-copy").hide();
+    $pre.children("code").prepend(button);
+    $pre.find(".clipboard-copy").hide();
   }
 }
 
-function toggleCopyButton(pre) {
-  pre.on("mouseenter", function() {
-    pre.find(".clipboard-copy").show();
+function toggleCopyButton($pre) {
+  $pre.on("mouseenter", function() {
+    $pre.find(".clipboard-copy").show();
   });
 
-  pre.on("mouseleave", function() {
-    pre.find(".clipboard-copy").hide();
+  $pre.on("mouseleave", function() {
+    $pre.find(".clipboard-copy").hide();
   });
 }
 
