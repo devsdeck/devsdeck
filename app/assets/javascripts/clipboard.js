0ed565c33511ae6ca65a674fba16e29d1ea28091
hosseintoussi <sltoussi@gmail.com>
2018-12-26 18:56:16 +0100

    make copy button always stick around

--- app/assets/javascripts/clipboard.js
+++ app/assets/javascripts/clipboard.js
@@ -9,30 +9,17 @@ function addCopyButton() {
     var id = "id_" + index;
     var $this = $(this)
     $this.children("code").attr("id", id);
-    prependButtonAndHide($this, id);
-    toggleCopyButton($this);
+    prependButton($this, id);
   });
 }
 
-function prependButtonAndHide($pre, id) {
+function prependButton($pre, id) {
   if ($pre.find(".clipboard-copy").length === 0) {
-    var button = $("<div class='ui small basic icon buttons right floated'>" +
+    var button = $("<div class='ui compact small basic icon buttons right floated'>" +
       "<button class='mini ui button clipboard-copy' data-clipboard-target=#" + id + ">" +
       "<i class='clipboard icon'></i>" +
       "</button>" +
       "</div>");
     $pre.children("code").prepend(button);
-    $pre.find(".clipboard-copy").hide();
   }
 }
-
-function toggleCopyButton($pre) {
-  $pre.on("mouseenter", function() {
-    $pre.find(".clipboard-copy").show();
-  });
-
-  $pre.on("mouseleave", function() {
-    $pre.find(".clipboard-copy").hide();
-  });
-}
-
